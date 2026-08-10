b96ff5095b15178a457620168a9c37d228c45152
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-08-10 18:11:28 +0530

    tags up

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,13 +63,30 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${outfit.variable} antialiased`}>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WH9MDGHR');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* Google Ads - First Account (AW-17773805096) */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-17773805096"
           strategy="afterInteractive"
         />
         <Script
-          id="google-analytics"
+          id="google-analytics-1"
           strategy="afterInteractive"
         >
           {`
@@ -79,6 +96,8 @@ export default function RootLayout({
             gtag('config', 'AW-17773805096');
           `}
         </Script>
+
+        {/* Google Ads - Second Account (AW-17571736679) */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-17571736679"
@@ -95,8 +114,33 @@ export default function RootLayout({
             gtag('config', 'AW-17571736679');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script
+          id="clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qgqxshf78a");
+            `,
+          }}
+        />
       </head>
       <body className="font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WH9MDGHR"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
